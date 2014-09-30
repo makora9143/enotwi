3994834f4160d2bd34bdca2b9b92405c40db5f28
Makoto Kawano <kawano_makoto@cyberagent.co.jp>
2014-09-30 20:04:52 +0900

    add temp comment

--- mecabhelper.py
+++ mecabhelper.py
@@ -1,32 +1,39 @@
 #! /usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-"""
+u"""MecabHelpler MeCabで名詞を抜き出すためのクラス
+	
+
 
 """
 
 from MeCab import Tagger
 
 class MecabHelpler(object):
-	"""docstring for MecabHelpler"""
+	u"""docstring for MecabHelpler"""
 	def __init__(self, dic="mecabrc"):
 		self.mecab = Tagger(dic)
 		self._data = []
 		self._words = {}
 
 	def add_text(self, text):
+		u"""text一文を追加する"""
 		self._data.append(text)
 
 	def replace_texts(self, texts):
+		u"""text複数文を差し替える"""
 		self._data = texts
 
 	def get_texts(self):
+		u"""保持している文を返す"""
 		return self._data
 
 	def parse_to_node(self):
+		u"""形態素解析する"""
 		return self.mecab.parseToNode('\n'.join(self._data))
 
 	def count_words(self):
+		u"""単語の頻出数をカウントする"""
 		node = self.parse_to_node()
 		while node:
 		    word = node.surface
@@ -37,8 +44,10 @@ class MecabHelpler(object):
     		node = node.next
 
 	def show_result(self): 
+		u"""結果を表示する"""
 		for word,count in self._words.items():
     		print word, count	
 
    	def get_result(self):
+   		u"""結果を返す"""
    		return self._words
